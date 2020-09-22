d62d241cc8fe0cd7faeb4bad8cf8dc992834d038
Nikhil <nikhilpathania099@gmail.com>
2020-09-22 20:13:09 +0530

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-
+import Spinner from './Spinner';
+import { render } from '@testing-library/react';
 
 
 class App extends React.Component{
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
+   
+    state={lat:null,errorMessage:''};
 
-            (position) => console.log(position),
-            (err) => console.log(err)
+    componentDidMount()
+    {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat:position.coords.latitude}),
+            err => this.setState({errorMessage:err.message})
         );
-        return(
-            <div >
-                <h4>Latitude:</h4>
+    }
+
+    renderContent(){
+        
+        if(this.state.errorMessage && ! this.state.errorMessage)
+        {
+        return<div>Error:{this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+        return <SeasonDisplay lat={this.state.lat}/>
+    }
+        return <Spinner message="Please accept the request"/>;
+    }
+    render(){
+        return (
+            <div className="borfer red">
+                {this.renderContent()}
             </div>
         );
     }
 }
+
+
 
 
 ReactDOM.render(<App /> ,document.querySelector('#root'));
